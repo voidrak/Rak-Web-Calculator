80d5212def07a2e4595826038367150d525c1aa8
voidrak <112881704+voidrak@users.noreply.github.com>
2023-08-07 00:57:32 +0300

    update

--- main.js
+++ main.js
@@ -1,6 +1,53 @@
-let numberBtn = document.querySelectorAll("numbers");
-let dashboard = document.getElementById("dashboard-input");
+const clear = document.getElementById("clear");
+const dashboard = document.getElementById("dashboard");
+const numBtn = document.querySelectorAll(".numbers");
+const opBtn = document.querySelectorAll(".operator");
+const equal = document.getElementById("equal");
+let arrayForCalculation = [];
+let sum = 0;
 
-numberBtn.style.color = "red";
-// numberBtn.addEventListener("onclick", function () {
-// });
+function add() {
+  for (let i = 0; i < arrayForCalculation.length; i++) {
+    sum += arrayForCalculation[i];
+  }
+  console.log(sum);
+}
+
+// clear the dashboard
+clear.addEventListener("click", (event) => {
+  dashboard.textContent = " ";
+  arrayForCalculation = [];
+  sum = 0;
+  console.log(arrayForCalculation);
+});
+
+//function to write to dashboard
+function writeToDashboard(element) {
+  element.forEach((foreach) => {
+    foreach.addEventListener("click", (event) => {
+      dashboard.textContent += event.target.textContent;
+    });
+  });
+}
+
+// to write numbers to dashboard from numbers
+writeToDashboard(numBtn);
+
+// to write numbers to dashboard from operator
+opBtn.forEach((foreach) => {
+  foreach.addEventListener("click", (event) => {
+    // push number to array
+    arrayForCalculation.push(JSON.parse(dashboard.textContent));
+    // change  the dashboard to clicked sign
+    // dashboard.textContent = event.target.textContent;
+    dashboard.textContent = " ";
+
+    console.log(arrayForCalculation);
+  });
+});
+
+// operation for equal sign
+equal.addEventListener("click", (event) => {
+  add();
+  dashboard.textContent = sum;
+});
